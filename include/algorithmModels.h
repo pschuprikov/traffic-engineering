52407d64c1e32cc1771403250bdfc4aead067541
Andrey Shein <andrey.shein.spb@gmail.com>
2021-12-15 21:09:00 +0300

    add entites for algorithm purposes

---
+++ include/algorithmModels.h
@@ -0,0 +1,47 @@
+#pragma once
+
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
+
+namespace TrafficEngineering {
+
+struct Edge {
+    std::string from;
+    std::string to;
+    int weight;
+};
+
+
+class Graph {
+public:
+    Graph(int vertex_number, const std::vector<Edge> edges);
+
+    int theShortestPathWeight(const std::string &from, const std::string &to);
+
+    // returns the unweighted path
+    std::vector<Edge> theShortestPath(const std::string &from, const std::string &to);
+private:
+    static constexpr int INF = 1000 * 1000 * 1000;
+private:
+    std::vector<std::string> _names;
+    std::unordered_map<std::string, int> _indexes;
+    std::vector<std::vector<int>> _distances;
+    std::vector<std::vector<int>> _parent;
+};
+
+
+class Tree {
+public:
+    Tree(const std::string &root);
+
+    void addEdge(const Edge &edge);
+
+    int theLongestPathWeight(const std::string &node);
+
+private:
+    std::unordered_map<std::string, std::vector<Edge>> _edges;
+};
+
+} // namespace TrafficEngineering
---
+++ src/algorithmModels.cc
@@ -0,0 +1,78 @@
+#include "algorithmModels.h"
+
+
+namespace TrafficEngineering {
+
+Graph::Graph(int vertex_number, const std::vector<Edge> edges) :
+    _names(vertex_number),
+    _distances(vertex_number, std::vector<int>(vertex_number, INF)),
+    _parent(vertex_number, std::vector<int>(vertex_number))
+{
+    for (const auto &edge : edges) {
+        if (_indexes.count(edge.from) == 0) {
+            _names[_indexes.size()] = edge.from;
+            _indexes[edge.from] = _indexes.size();
+        }
+        if (_indexes.count(edge.to) == 0) {
+            _names[_indexes.size()] = edge.to;
+            _indexes[edge.to] = _indexes.size();
+        }
+        _distances[_indexes[edge.from]][_indexes[edge.to]] = edge.weight;
+        _parent[_indexes[edge.from]][_indexes[edge.to]] = _indexes[edge.from];
+    }
+    for (int i = 0; i < vertex_number; i++) {
+        _distances[i][i] = 0;
+        _parent[i][i] = i;
+    }
+
+    for (int k = 0; k < vertex_number; k++) {
+        for (int i = 0; i < vertex_number; i++) {
+            for (int j = 0; j < vertex_number; j++) {
+                if (_distances[i][j] > _distances[i][k] + _distances[k][j]) {
+                    _distances[i][j] = _distances[i][k] + _distances[k][j];
+                    _parent[i][j] = k;
+                }
+            }
+        }
+    }
+}
+
+int Graph::theShortestPathWeight(const std::string &from, const std::string &to) {
+    assert(_indexes.count(from) != 0);
+    assert(_indexes.count(to) != 0);
+    return _distances[_indexes[from]][_indexes[to]];
+}
+
+std::vector<Edge> Graph::theShortestPath(const std::string &from, const std::string &to) {
+    assert(_indexes.count(from) != 0);
+    assert(_indexes.count(to) != 0);
+    std::string vertex = _names[_parent[_indexes[from]][_indexes[to]]];
+    if (from == vertex) {
+        return {{from, to, 0}};
+    }
+    auto left = theShortestPath(from, vertex);
+    auto right = theShortestPath(vertex, to);
+    left.insert(left.end(), right.begin(), right.end());
+    return left;
+}
+
+
+Tree::Tree(const std::string &root) {
+    _edges[root] = {};
+}
+
+void Tree::addEdge(const Edge &edge) {
+    assert(_edges.count(edge.from) != 0);
+    _edges[edge.from].push_back(edge);
+}
+
+int Tree::theLongestPathWeight(const std::string &node) {
+    assert(_edges.count(node) != 0);
+    int result = 0;
+    for (const auto &edge : _edges[node]) {
+        result = std::max(result, edge.weight + theLongestPathWeight(edge.to));
+    }
+    return result;
+}
+
+} // namespace TrafficEngineering
