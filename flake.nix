eac938c25045ce4907387428cc47ac99a6ecbeee
Pavel Chuprikov <pschuprikov@gmail.com>
2025-07-23 20:11:29 +0200

    update flake to 6.0

--- flake.nix
+++ flake.nix
@@ -3,18 +3,18 @@
 
   inputs.flake-utils.url = github:numtide/flake-utils;
   inputs.nur.url = github:pschuprikov/nur-packages;
-  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-21.11;
+  inputs.nixpkgs.url = github:NixOS/nixpkgs/nixos-25.05;
 
   outputs = { self, flake-utils, nur, nixpkgs }:
     flake-utils.lib.eachDefaultSystem (system: 
       let 
         pkgs = nixpkgs.legacyPackages.${system};
-        nurpkgs = import nur { inherit pkgs; }; in {
+        nurpkgs = nur.packages.${system}; in {
       devShell = pkgs.mkShell {
-        buildInputs = [ nurpkgs.qt5.omnetpp nurpkgs.qt5.omnetpp-inet ];
+        buildInputs = [ nurpkgs.omnetpp_6_0 nurpkgs.omnetpp-inet_4_3_9 ];
         shellHook = ''
-          export INET_INCLUDE=${nurpkgs.qt5.omnetpp-inet}/include
-          export INET_LDPATH=${nurpkgs.qt5.omnetpp-inet}/lib
+          export INET_INCLUDE=${nurpkgs.omnetpp-inet_4_3_9}/include
+          export INET_LDPATH=${nurpkgs.omnetpp-inet_4_3_9}/lib
         '';
       };
     });
